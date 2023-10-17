0f140dc18c6d1c823441a25eb9c82e25bfd7d394
Damian Traczuk <damiantra7@gmail.com>
2023-10-17 18:17:45 +0200

    lab4 done

---
+++ MemoryGame/ThemeButton.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButton.swift
+//  MemoryGame
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ThemeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButton()
+}
